f3c7a4da7debd52f1639cb057fba0baa88b6905f
Ryan Muskopf <ryanjmuskopf@gmail.com>
2017-02-08 02:09:43 -0500

    added web sockets

--- src/server.js
+++ src/server.js
@@ -16,3 +16,33 @@ const onRequest = (request, response) => {
 const app = http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
+
+// pass int he http server to socketio and grab the websocker server as io
+const io = socketio(http);
+
+// objsect to store all users
+const users = {};
+
+// delegate fucntions
+const onJoined = (sock) => {
+  const socket = sock;
+};
+
+const onMsg = (sock) => {
+  const socket = sock;
+};
+
+const onDisconnect = (sock) => {
+  const socket = sock;
+};
+
+
+io.sockets.on('connection', (socket) => {
+  console.log('started');
+
+  onJoined(socket);
+  onMsg(socket);
+  onDisconnect(socket);
+});
+
+console.log('Websocket server started');

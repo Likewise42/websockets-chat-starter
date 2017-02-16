d19d8c4c767efff3f1cb8483ae1210888c7543c2
Ryan Muskopf <ryanjmuskopf@gmail.com>
2017-02-16 02:32:59 -0500

    Server is now working.

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const app = http.createServer(onRequest).listen(port);
 console.log(`Listening on 127.0.0.1: ${port}`);
 
 // pass int he http server to socketio and grab the websocker server as io
-const io = socketio(http);
+const io = socketio(app);
 
 // objsect to store all users
 const users = {};
@@ -26,14 +26,71 @@ const users = {};
 // delegate fucntions
 const onJoined = (sock) => {
   const socket = sock;
+  socket.on('join', (data) => {
+    //message back to new user
+    const joinMsg ={
+      name:'server',
+      msg: `There are ${Object.keys(users).length} other users online`,
+    };
+    
+    socket.name = data.name;
+    socket.emit('msg', joinMsg);
+    
+    //check if name alreay in use
+    let uName = true;
+    let nameChanged = false;
+    let nextUserNum = 0;
+    const originalName = socket.name;
+    do {
+      if (users[socket.name]){
+        uName = false;
+        nameChanged = true;
+        nextUserNum++;
+        socket.name = originalName+nextUserNum;
+      }else{
+        uName = true;
+      }
+    } while (!uName);
+  
+    if(nameChanged){
+      socket.emit('msg', {name: 'server',msg:`Your name was not unique and was changed to ${socket.name}.`})
+    }
+  
+    users[socket.name] = socket;
+    
+    socket.join('room1');
+    
+    //announcemnt
+    const response ={
+      name: 'server',
+      msg: `${socket.name} has joined the room.`,
+    };
+    socket.broadcast.to('room1').emit('msg',response);
+    
+    
+    console.log(`${socket.name} joined`);
+    
+    //success message backto the server
+    socket.emit('msg', {name:'server',msg:'You joined the room'})
+  });
 };
 
 const onMsg = (sock) => {
   const socket = sock;
+  
+  socket.on('msgToServer', (data) =>{
+    io.sockets.in('room1').emit('msg', {name: socket.name, msg:data.msg});
+  });
 };
 
 const onDisconnect = (sock) => {
   const socket = sock;
+  
+  socket.on('disconnect', (data) =>{
+    delete users[socket.name];
+    io.sockets.in('room1').emit('msg', {name: 'server', msg:`User ${socket.name} disconnected`});
+    console.log("disconnect detected from: " + socket.name +". "+Object.keys(users).length+" users remain.");
+  });
 };
 
 
